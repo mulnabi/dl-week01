9d3262168741da0ba0a14641c1d931738c377ca4
nolr <nolr3@naver.com>
2026-03-09 22:59:06 +0900

    Auto commit: 2026-03-09 22:59 - 저장됨

--- week01/python_basic.py
+++ week01/python_basic.py
@@ -392,4 +392,22 @@ print(sorted('Python'))
 # Request('http://www.sunmoon.ac.kr')   # 오류 발생
 
 import urllib.request
+urllib.request.Request('http://www.sunmoon.ac.kr')
 
+import pandas
+pandas.DataFrame()
+
+from datetime import datetime
+datetime.now()
+
+# 파일 객체 생성
+f = open('example.txt', 'w')
+print(f)
+
+# 파일 닫기
+f.close()
+
+# 파일 쓰기
+f = open('example.txt', 'w')
+for i in range(1, 6):
+    
