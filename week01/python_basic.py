07c1c8eb7bf5016d5f93d476a6b13907bd6075bd
nolr <nolr3@naver.com>
2026-03-09 22:21:27 +0900

    Auto commit: 2026-03-09 22:21 - 저장됨

--- week01/python_basic.py
+++ week01/python_basic.py
@@ -261,3 +261,7 @@ if money >=1200 and money <3500:
 # 그룹 자료형의 원소인지 검사하기
 print(1 in [1, 2, 3])
 print(x in [1, 2, 3])
+print(x not in [1, 2, 3])
+print('a' in ['a', 'b', 'c', 'd'])
+print('i' not in 'Python')
+
