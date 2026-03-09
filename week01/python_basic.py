463997486485516000c16cbc5dcdbde37dbf8fb2
nolr <nolr3@naver.com>
2026-03-09 18:35:29 +0900

    Auto commit: 2026-03-09 18:35 - 저장됨

--- week01/python_basic.py
+++ week01/python_basic.py
@@ -53,3 +53,12 @@ print(a.find('y'))
 print(a.find('p'))
 print(a.index('y'))
 # print(a.index('p')) # 오류 발생
+
+# 문자 삽입
+b = ","
+c = b.join('Abcd')
+print(c)
+
+# 대소문자 변환
+print(a.upper())
+print(a.lower())
