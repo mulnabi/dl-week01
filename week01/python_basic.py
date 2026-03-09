94f621cef81da7a01dc40dd292ef76c5da5be161
nolr <nolr3@naver.com>
2026-03-09 22:04:40 +0900

    Auto commit: 2026-03-09 22:04 - 저장됨

--- week01/python_basic.py
+++ week01/python_basic.py
@@ -217,3 +217,11 @@ s2 = set([1, 2, 3, 4, 5, 6])
 print(s2)
 s3 = set([4, 5, 6, 7, 8, 9])
 print(s3)
+
+# 교집합 연산
+print(s2 & s3)
+print(s2.intersection(s3))
+
+# 합짚합 연산
+print(s2 | s3)
+print(s2.union(s3))
