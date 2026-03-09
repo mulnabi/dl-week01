b3056aa0f7a1c80f91a692ce15aba06ea591af51
nolr <nolr3@naver.com>
2026-03-09 22:43:13 +0900

    Auto commit: 2026-03-09 22:43 - 저장됨

--- week01/python_basic.py
+++ week01/python_basic.py
@@ -328,3 +328,15 @@ print(chr(97))
 print(chr(48))
 
 # 문자에 대한 아스키코드 값 출력
+print(ord('a'))
+print(ord('0'))
+
+# 객체 x가 가진 멤버 변수와 멤버 함수 보여주기
+print(dir([1, 2, 3]))
+print(dir({'1':'a'}))
+print(dir(1))
+
+print(int('3'))
+print(str(3))
+
+# x를 리스트로 반환
