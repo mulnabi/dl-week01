1ced9cb3457c2de6f5dfb0da0c619e92fd48149d
nolr <nolr3@naver.com>
2026-03-09 22:25:01 +0900

    Auto commit: 2026-03-09 22:25 - 저장됨

--- week01/python_basic.py
+++ week01/python_basic.py
@@ -274,3 +274,15 @@ else:
 """# 4. 반복문"""
 
 # for 반복문1
+test_list = ['one', 'two', 'three']
+for i in test_list:
+    x = i + '!'
+    print(x)
+
+# for 반복문2
+number = 0
+for score in [90, 25, 67, 45, 93]:
+    number += 1
+    
+    if score > 60:
+        
