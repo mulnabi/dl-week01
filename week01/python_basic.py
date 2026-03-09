14cb72a2b3924eaa272ad0056dd5bffa9bcaba4b
nolr <nolr3@naver.com>
2026-03-09 23:03:32 +0900

    Auto commit: 2026-03-09 23:03 - 저장됨

--- week01/python_basic.py
+++ week01/python_basic.py
@@ -417,7 +417,9 @@ f.close()
 # 파일에 내용 추가하기
 f = open('example.txt', 'w')
 for i in range(6, 11):
-    data = 
+    data = '%d번째 줄 추가입니다. \n' % i
+    f.write(data)
+f.close()
 
 # 파일 모드 'r' - readline()
 f = open('example.txt', 'r')
