70d58c67593a3337a7fbaa8a9d8fc6c01b83317e
nolr <nolr3@naver.com>
2026-03-22 13:41:16 +0900

    Auto commit: 2026-03-22 13:41 - 저장됨

--- week02/pandas_numpy.py
+++ week02/pandas_numpy.py
@@ -49,4 +49,11 @@ print(netflix.head())
 print(netflix['release_year'])
 print(netflix['release_year'] > 2015)
 
-more
+more2015 = netflix[netflix['release_year'] > 2015]
+print(more2015.head(10))
+
+print(~(netflix['release_year'] > 2015))
+less2015 = netflix[~(netflix['release_year'] > 2015)]
+print(less2015.head())
+
+print()
