6d6b1b6935c5236bd86a28f8a2a74162f61858ad
nolr <nolr3@naver.com>
2026-03-22 13:18:21 +0900

    Auto commit: 2026-03-22 13:18 - 저장됨

--- week02/pandas_numpy.py
+++ week02/pandas_numpy.py
@@ -11,3 +11,8 @@ sample.describe()
 
 sample_dic = {'name': ['John','Ann','kevin'], 'age': [23,22,21]}
 a = pd.DataFrame(sample_dic)
+
+a.info()
+
+pd.DataFrame([[1,2,],[3,4],[5,6],[7,8]])
+pd.DataFrame([[1,2],[3,4],[5,6],[7,8]], columns=)
