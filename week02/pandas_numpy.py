c5f4bd80246315507434664ba38a3bd5dbca3135
nolr <nolr3@naver.com>
2026-03-22 13:11:18 +0900

    Auto commit: 2026-03-22 13:11 - 저장됨

--- week02/pandas_numpy.py
+++ week02/pandas_numpy.py
@@ -1,3 +1,3 @@
 import pandas as pd
 
-file_url = 'https://media.githubusercontent.com/media/musthave-ML10/data_source/'
+file_url = 'https://media.githubusercontent.com/media/musthave-ML10/data_source/main/'
