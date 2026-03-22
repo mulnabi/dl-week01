769bc92fa866453dbdb7a7ef7c05ae58b6b30636
nolr <nolr3@naver.com>
2026-03-22 14:48:38 +0900

    Auto commit: 2026-03-22 14:48 - 저장됨

--- week02/pandas_numpy.py
+++ week02/pandas_numpy.py
@@ -141,3 +141,8 @@ df = pd.DataFrame(data)
 print(df.head())
 
 # 개인별 과목 점수의 평균값 계산 (axis=1)
+df['average'] = df[['math', 'science', 'english']].mean(axis=1)
+print(df)
+
+# 이름과 평균값만을 포함하는 새로운 데이터프레임 생성
+a
