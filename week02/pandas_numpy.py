0761c47fe12fd9dc948fdb599567414765fef9ca
nolr <nolr3@naver.com>
2026-03-22 13:53:49 +0900

    Auto commit: 2026-03-22 13:53 - 저장됨

--- week02/pandas_numpy.py
+++ week02/pandas_numpy.py
@@ -77,5 +77,6 @@ print(df.head())
 # 필터링 조건 설정
 condition = {
     (df['comment_length'] >= 100) &       # 댓글 길이 100자 이상
-    (df['likes'])
+    (df['likes'] >= 20) &                 # 좋아요 20개 이상
+    (~)
 }
