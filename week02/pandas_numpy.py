cbc6f69593725ec5308a5bb72e88af63125e517a
nolr <nolr3@naver.com>
2026-03-22 17:36:18 +0900

    Auto commit: 2026-03-22 17:36 - 저장됨

--- week02/pandas_numpy.py
+++ week02/pandas_numpy.py
@@ -78,7 +78,7 @@ print(df.head())
 condition = {
     (df['comment_length'] >= 100) &       # 댓글 길이 100자 이상
     (df['likes'] >= 20) &                 # 좋아요 20개 이상
-    (~df['is_sapm']) &                    # 스팸 댓글이 아니어야 함
+    (~df['is_spam']) &                    # 스팸 댓글이 아니어야 함
     (df['has_image'])                     # 이미지가 포함된 댓글이어야 함
 }
 
