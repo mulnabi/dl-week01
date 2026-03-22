652b4d5281db3973171d260df811f6fe29954866
nolr <nolr3@naver.com>
2026-03-22 13:30:01 +0900

    Auto commit: 2026-03-22 13:30 - 저장됨

--- week02/pandas_numpy.py
+++ week02/pandas_numpy.py
@@ -41,4 +41,9 @@ print(sample_df[0:3, 2:4])
 
 print(sample_df.drop(['var_1','var_3'], axis=1))
 print(sample_df.drop(['var_1','var_2'], axis=1))
-print(sample_df.drop(['a','b','c'], axis=1))
+print(sample_df.drop(['a','b','c'], axis=0))
+
+netflix = pd.read_csv('2.1.1.netflix.csv')
+print(netflix.head())
+
+print(netflix[])
