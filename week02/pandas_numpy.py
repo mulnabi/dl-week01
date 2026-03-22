a027761b0ba1d8e455b5ce698f89f3474744c55e
nolr <nolr3@naver.com>
2026-03-22 13:23:42 +0900

    Auto commit: 2026-03-22 13:23 - 저장됨

--- week02/pandas_numpy.py
+++ week02/pandas_numpy.py
@@ -20,5 +20,9 @@ pd.DataFrame([[1,2],[3,4],[5,6],[7,8]], columns=['var_1','var_2'], index=['a','b
 import pandas as pd
 file_url = 'https://media.githubusercontent.com/media/musthave-ML10/data_source/main/sample_df.csv'
 
-sample = pd.read_csv(file_url, index_col=0)
-print(sample_)
+sample_df = pd.read_csv(file_url, index_col=0)
+print(sample_df.head())
+
+print(sample_df['var_5'])
+
+# print(sample_df['var_1', 'var_2'])
