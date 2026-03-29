592e4ffb51643eae6a6b8e01cfa3e7abf9a90960
nolr <nolr3@naver.com>
2026-03-29 21:39:32 +0900

    Auto commit: 2026-03-29 21:39 - 저장됨

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -95,4 +95,4 @@ plt.title('Age and Fare Relationship with Survival on the Titanic')
 plt.xlabel('Age')
 plt.ylabel('Fare')
 plt.legend(handles=scatter.legend_elements()[0], title='Survived',
-           labels)
+           labels=[])
