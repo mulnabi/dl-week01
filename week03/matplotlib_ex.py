47ce9941e9d11fa0310b2f5c7ccadf7351d8b1d6
nolr <nolr3@naver.com>
2026-03-29 21:28:01 +0900

    Auto commit: 2026-03-29 21:28 - 저장됨

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -68,3 +68,6 @@ plt.title('Survived Counts by Gender on Titanic')
 plt.xlabel('Count')
 plt.ylabel('gender')
 plt.legend(bars, ['Survived - Female', 'Survived - Male'], loc='upper right')
+
+# 차이 강조를 위해 수평선 추가
+plt.axvline(x=survived_counts['male'])
