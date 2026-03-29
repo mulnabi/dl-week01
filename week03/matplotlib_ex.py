47b5f9c724dedd70fb3609644198d62a7d58845b
nolr <nolr3@naver.com>
2026-03-29 21:25:06 +0900

    Auto commit: 2026-03-29 21:25 - 저장됨

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -57,3 +57,11 @@ plt.savefig('Figure02.png')
 plt.close()
 
 """### **수평 막대 그래프 : 성별에 따른 생존자 수 확인하기**"""
+
+## 성별에 따른 생존자 수 계산
+survived_counts = titanic[titanic['Survived'] == 1]['Sex'].value_counts()
+print(survived_counts)
+
+# 수평 막대 그래프 그리기
+bars = plt.barh(survived_counts.index, survived_counts, color=['darkturquoise', 'salmon'])
+plt.title('Survived Counts by Gender on Titinic')
