a8c7a393365d7ba81bd2b918247f671b043204c9
nolr <nolr3@naver.com>
2026-03-29 22:45:43 +0900

    Auto commit: 2026-03-29 22:45 - 저장됨

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -248,3 +248,13 @@ print(fare_std)
 plt.figure(figsize=(10, 6))
 
 # 에러바 생성
+plt.errorbar(fare_means.index, fare_means, yerr=fare_std, fmt='o',
+             capsize=5, capthick=1, label='Fare')
+
+plt.title('Error Bar Plot of Fare by Parch')
+plt.xlabel('Parch')
+plt.ylabel('Fare')
+plt.xticks(fare_means.index)
+plt.legend()
+plt.savefig('figure11.png')
+plt.close()
