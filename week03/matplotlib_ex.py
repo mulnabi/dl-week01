5ccb670494d1559246d0fb573a84fc25daf3f6ba
nolr <nolr3@naver.com>
2026-03-29 23:34:15 +0900

    Auto commit: 2026-03-29 23:34 - 저장됨

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -389,4 +389,23 @@ ax2 = ax1.twinx()
 
 # 두 번째 그래프 설정 (막대 그래프)
 ax1.bar(x, y2, color='deeppink', alpha=0.7, width=0.7, label='Survived')
+ax2.set_ylabel('Survived Count', color='deeppink') # 서브플롯 y축에 레이블 추가
+ax2.tick_params(axis='y', labelcolor='deeppink')
+ax2.legend(loc='upper right', bbox_to_anchor=(1, 0.9))
 
+plt.suptitle('Survival Analysis by Number of Parents/Children (Parch) on the Titanic')
+plt.tight_layout()
+plt.savefig('Figure16.png')
+plt.close()
+
+## **그래프 저장하기**
+
+# 히스토그램 그리기
+plt.hist(titanic['Age'], bins=20, color='skyblue', edgecolor='black')
+plt.xlabel('Age')
+plt.ylabel('Count')
+plt.title('Distribution of Ages on the Titanic')
+plt.grid(axis='y', linestyle='--', alpha=0.7)
+
+# 이미지로 저장
+plt.savefig('Figure17.png')
