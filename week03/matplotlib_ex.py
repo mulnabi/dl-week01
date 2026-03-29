d1f59f2967ed667651b728514d1d4ef791f3b30d
nolr <nolr3@naver.com>
2026-03-29 21:25:14 +0900

    Auto commit: 2026-03-29 21:25 - 저장됨

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -64,4 +64,4 @@ print(survived_counts)
 
 # 수평 막대 그래프 그리기
 bars = plt.barh(survived_counts.index, survived_counts, color=['darkturquoise', 'salmon'])
-plt.title('Survived Counts by Gender on Titinic')
+plt.title('Survived Counts by Gender on Titanic')
