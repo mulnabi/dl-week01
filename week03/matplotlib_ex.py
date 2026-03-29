7925c28585014c35e75aa62deb0daaadc3a9641e
nolr <nolr3@naver.com>
2026-03-29 21:27:01 +0900

    Auto commit: 2026-03-29 21:27 - 저장됨

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -67,4 +67,4 @@ bars = plt.barh(survived_counts.index, survived_counts, color=['darkturquoise', 
 plt.title('Survived Counts by Gender on Titanic')
 plt.xlabel('Count')
 plt.ylabel('gender')
-plt.legend(bars, ['Survived - Female', 'Survived - Male'], loc='')
+plt.legend(bars, ['Survived - Female', 'Survived - Male'], loc='upper right')
