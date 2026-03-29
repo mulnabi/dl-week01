1a34043afb106fdf697ed38f339a22a734953709
nolr <nolr3@naver.com>
2026-03-29 22:14:08 +0900

    Auto commit: 2026-03-29 22:14 - 저장됨

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -184,4 +184,14 @@ plt.ylabel('Count')
 plt.xticks(rotation=45)
 plt.legend()
 plt.grid(True)
-plt
+plt.savefig('Figure08.png')
+plt.close()
+
+## **박스 플롯 : 승객 나이의 데이터 분포, 중앙값, 이상치 살펴보기**
+
+# 결측치 처리
+titanic = titanic.dropna(subset=['Age'])
+print(titanic.info())
+
+# 승객 등급에 따른 나이의 박스 플롯
+plt.boxplot([titanic[]])
