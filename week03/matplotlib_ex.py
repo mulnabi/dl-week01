6ace2c0bedea0bae0af8a2cd836b71cae76918f4
nolr <nolr3@naver.com>
2026-03-29 23:26:44 +0900

    Auto commit: 2026-03-29 23:26 - 저장됨

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -373,3 +373,9 @@ x = parch_counts.index.astype(str) # 데이터프레임의 인덱스를 문자�
 y1 = parch_counts[0].values # 사망자
 y2 = parch_counts[1].values # 생존자
 
+# plt.subplots() 함수를 사용하여 그래프 생성
+# figure와 서브플롯 생성
+fig, ax1 = plt.subplots()
+
+# 첫 번째 그래프 설정 (선 그래프)
+ax1.plot(x, y1, '-s', color='indigo', markersize=7, linewidth=5, alpha=0.7, label='Not Survived')
