522336c04fcd9ef79f6cf3e835806679ce681841
nolr <nolr3@naver.com>
2026-03-29 23:40:38 +0900

    Auto commit: 2026-03-29 23:40 - 저장됨

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -107,7 +107,7 @@ print(survived_counts)
 
 # 파이 차트 그리기
 plt.figure(figsize=(8, 8))
-plt.pie(survived_counts, lables=['Not Survived', 'Survived'], colors=['orange', 'gold'],
+plt.pie(survived_counts, labels=['Not Survived', 'Survived'], colors=['orange', 'gold'],
         autopct='%0.1f%%', startangle=90, shadow=True, explode=(0, 0.1))
 
 plt.title('Survival Distribution on the Titanic')
