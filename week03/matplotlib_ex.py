db39976c283b226f248661fde3bb061e985c9dbc
nolr <nolr3@naver.com>
2026-03-29 23:24:13 +0900

    Auto commit: 2026-03-29 23:24 - 저장됨

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -359,3 +359,7 @@ axes[1].legend(loc='upper right')
 # 제목 설정
 fig.suptitle('Survival Analysis by Number of Parents/Children (Parch) on the Titanic')
 fig.tight_layout()
+plt.savefig('figure15.png')
+plt.close()
+
+## **
