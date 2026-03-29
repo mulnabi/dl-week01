57b2a920fa262fe886c3edb822ac1ba3a0de6f42
nolr <nolr3@naver.com>
2026-03-29 22:11:52 +0900

    Auto commit: 2026-03-29 22:11 - 저장됨

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -171,4 +171,17 @@ print(survived_counts)
 plt.figure(figsize=(10, 6))
 
 # 나이대별 생존자
-plt.fill_between(survived_counts.index.astype(str), )
+plt.fill_between(survived_counts.index.astype(str), survived_counts[1],
+                 color='purple', alpha=0.9, label='Survived')
+
+# 나이대별 사망자
+plt.fill_between(survived_counts.index.astype(str), survived_counts[0],
+                 color='hotpink', alpha=0.6, label='Not Survived')
+
+plt.title('Survival by Age Group on titanic')
+plt.xlabel('Age')
+plt.ylabel('Count')
+plt.xticks(rotation=45)
+plt.legend()
+plt.grid(True)
+plt
