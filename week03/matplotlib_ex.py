2964a4e5b1babaddc05c95b33b3b1fb0504b37df
nolr <nolr3@naver.com>
2026-03-29 21:44:59 +0900

    Auto commit: 2026-03-29 21:44 - 저장됨

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -110,4 +110,4 @@ plt.figure(figsize=(8, 8))
 plt.pie(survived_counts, lables=['Not Survived', 'Survived'], colors=['orange', 'gold'],
         autopct='%0.1f%%', startangle=90, shadow=True, explode=(0, 0.1))
 
-plt.title('Survival Distaribution')
+plt.title('Survival Distribution')
