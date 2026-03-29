5b737eae24be2fa8d4722ccbfd6a941d8d481ce0
nolr <nolr3@naver.com>
2026-03-29 21:39:22 +0900

    Auto commit: 2026-03-29 21:39 - 저장됨

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -89,4 +89,10 @@ print(titanic.info())
 
 # 산점도 그래프 그리기
 plt.figure(figsize=(12, 8))
-scatter = plt
+scatter = plt.scatter(x='Age', y='Fare', data=titanic, c=titanic['Survived'],cmap='Set2', alpha=0.7)
+
+plt.title('Age and Fare Relationship with Survival on the Titanic')
+plt.xlabel('Age')
+plt.ylabel('Fare')
+plt.legend(handles=scatter.legend_elements()[0], title='Survived',
+           labels)
