a6c99d40d0acd008403eb12f4b330ab620c9cb6a
nolr <nolr3@naver.com>
2026-03-29 22:49:51 +0900

    Auto commit: 2026-03-29 22:49 - 저장됨

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -273,3 +273,10 @@ plt.subplot(2, 2, 4)
 plt.plot([10, 11, 12])
 plt.savefig('Figure12.png')
 plt.close()
+
+## **타이타닉 데이터셋으로 개별 서브플롯 하나씩 그리기**
+
+# Survived가 0이면 사망자을, 1이면 생존자를 나타냄
+titanic = pd.read_csv('titanic.csv')
+
+
