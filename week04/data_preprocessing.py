bd4d635f38273dcf0dbc342b6d3d0b9c6981c177
nolr <nolr3@naver.com>
2026-04-05 22:13:18 +0900

    Auto commit: 2026-04-05 22:13 - 저장됨

--- week04/data_preprocessing.py
+++ week04/data_preprocessing.py
@@ -35,4 +35,32 @@ housing.loc[null_rows_idx].head()
 
 from sklearn.impute import SimpleImputer
 
-isola
+imputer = SimpleImputer(strategy="median")
+
+# 수치형 특성만 추출
+housing_num = housing.select_dtypes(include=[np.number])
+housing_num.head()
+
+imputer.fit(housing_num)
+
+print(imputer.statistics_)           # imputer 결과 값
+print(housing_num.median().values)   # 수동으로 계산한 중간값
+
+# 훈련 세트의 누락값을 imputer가 학습한 값으로 채우기
+X = imputer.transform(housing_num)
+
+imputer.feature_names_in_
+
+housing_tr = pd.DataFrame(X, columns=housing_num.columns,
+                          index=housing_num.index)
+housing_tr.loc[null_rows_idx].head()
+
+# 이상치 삭제
+from sklearn.ensemble import IsolationForest
+
+isolation_forest = IsolationForest(random_state=42)
+outlier_pred = isolation_forest.fit_predict(X)
+
+outlier_pred
+
+housing = housing.iloc[]
