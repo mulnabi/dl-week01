5b78b73da2d8d3249ddaa1fe58f4652a0b7a4330
nolr <nolr3@naver.com>
2026-04-05 22:00:27 +0900

    Auto commit: 2026-04-05 22:00 - 저장됨

--- week04/data_preprocessing.py
+++ week04/data_preprocessing.py
@@ -20,5 +20,7 @@ for set_ in (start_train_set, start_test_set):
     set_.drop("income_cat", axis=1, inplace=True)
 
 """
+* 원본 훈련 세트로 복원하고 타깃을 분리
+* 'start_train_set.drop()'은 지정한 열을 제외한 'start_train_set'의 복사본을 만듦
 
 """
