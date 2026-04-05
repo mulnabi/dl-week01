d7db52e802f1823ac9eb2c5f070a9ace83bd56ae
nolr <nolr3@naver.com>
2026-04-05 21:57:58 +0900

    Auto commit: 2026-04-05 21:57 - 저장됨

--- week04/data_preprocessing.py
+++ week04/data_preprocessing.py
@@ -5,3 +5,13 @@ import numpy as np
 import pandas as pd
 
 housing = pd.read_csv('../week04/housing.csv')       # 오류 발생 시, ./housing.csv 파일로도 시도
+
+# 테스트 세트 만들기
+from sklearn.model_selection import train_test_split
+
+housing["income_cat"] = pd.cut(housing["median_income"],
+                               bins=[0., 1.5, 3.0, 4.5, 6., np.inf],
+                               labels=[1, 2, 3, 4, 5])
+
+start_train_set, start_test_set = train_test_split(
+    housing, test_size=0.2, stratify=
