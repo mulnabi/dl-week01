120994f791acc4e3579ae87d072558aa551ddd5d
nolr <nolr3@naver.com>
2026-04-05 22:16:17 +0900

    Auto commit: 2026-04-05 22:16 - 저장됨

--- week04/data_preprocessing.py
+++ week04/data_preprocessing.py
@@ -72,4 +72,9 @@ housing_cat.head(8)
 
 from sklearn.preprocessing import OrdinalEncoder
 
-ordinal
+ordinal_encoder = OrdinalEncoder()
+housing_cat_encoded = ordinal_encoder.fit_transform(housing_cat)
+
+housing_cat_encoded[:8]
+
+ordinal_encoder.categories_
