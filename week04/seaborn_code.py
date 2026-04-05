2443fac073cd43c6d4a491c8f818d770261487e6
nolr <nolr3@naver.com>
2026-04-05 21:05:49 +0900

    Auto commit: 2026-04-05 21:05 - 저장됨

--- week04/seaborn_code.py
+++ week04/seaborn_code.py
@@ -41,4 +41,7 @@ sns.countplot(x='time', data=tips, ax=ax1)
 
 # 식사가 이루어진 시간대 파악과 식사가 이루어진 요일로 색상 분류
 # x축 변수, hue로 색상 분류, 데이터 셋, 색상 결정, axe 객체(2번째 그래프)
-sns.countplot
+sns.countplot(x='time', hue='day', data=tips, palette='Set2', ax=ax2)
+
+ax1.set_title('Frequency of Tips by Time')
+ax1.set_title('Frequency of Tips by Time')
