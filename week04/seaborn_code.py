a063d740c5745df90b567eec29127310396c7810
nolr <nolr3@naver.com>
2026-04-05 19:58:41 +0900

    Auto commit: 2026-04-05 19:58 - 저장됨

--- week04/seaborn_code.py
+++ week04/seaborn_code.py
@@ -3,4 +3,16 @@
 # 시본 라이브러리 불러오기
 import seaborn as sns
 
-# **팁()
+# **팁(tips) 데이터셋 불러오기**
+tips = sns.load_dataset('tips')
+print(tips.head())
+
+tips.info()
+
+# **범주형 변수 산점도 그래프**
+
+import matplotlib.pyplot as plt
+
+# figure에 2개의 서브 플롯을 생성
+fig = plt.figure(figsize=(15, 5))
+ax1 = fig.add_subplot(1, 2, 1)
