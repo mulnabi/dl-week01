7810e6683de3aada8560d36e3c33a75ac55fdc72
nolr <nolr3@naver.com>
2026-04-05 22:07:33 +0900

    Auto commit: 2026-04-05 22:07 - 저장됨

--- week04/data_preprocessing.py
+++ week04/data_preprocessing.py
@@ -32,3 +32,6 @@ housing_labels = start_train_set["median_house_value"].copy()
 # null 값이 있는 행 확인하기
 null_rows_idx = housing.isnull().any(axis=1)
 housing.loc[null_rows_idx].head()
+
+from sklearn.impute import SimpleImputer
+
