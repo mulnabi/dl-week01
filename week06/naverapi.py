17d0636fbba605189dc4b44dce5bf2b5ec71e6e7
nolr <nolr3@naver.com>
2026-04-15 22:13:17 +0900

    Auto commit: 2026-04-15 22:13 - 저장됨

--- week06/naverapi.py
+++ week06/naverapi.py
@@ -48,4 +48,4 @@ def getRequestUrl(url):
             return response.read().decode('utf-8')
     except Exception as e:
         print(e)
-        print("[%s] Error for U")
+        print("[%s] Error for URL : %s" % (datetime.datetime.now(), url))
