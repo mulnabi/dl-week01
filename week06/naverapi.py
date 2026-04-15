255d6c9d7878f1e1f5e27bc69733183c75fed943
nolr <nolr3@naver.com>
2026-04-15 22:07:11 +0900

    Auto commit: 2026-04-15 22:07 - 저장됨

--- week06/naverapi.py
+++ week06/naverapi.py
@@ -25,4 +25,15 @@ def main():
 def getNaverSearch(node, srcText, page_start, display):
     base = "https://openapi.naver.com/v1/search"
     node = "/%s.json" % node
-    parameters = "?query=%s&start=%s&display%s" % ()
+    parameters = "?query=%s&start=%s&display%s" % (urllib.parse.quote(srcText), page_start, display)
+
+    url = base + node + parameters
+    responseDecode = getRequestUrl(url)
+
+    if (responseDecode == None):
+        return None
+    elif:
+        return return json.loads(responseDecode)
+
+def getRequestUrl(url):
+    req
