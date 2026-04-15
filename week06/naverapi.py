6aaf708116bf7cda298e48a2de9af7625a78311d
nolr <nolr3@naver.com>
2026-04-15 22:30:54 +0900

    Auto commit: 2026-04-15 22:30 - 저장됨

--- week06/naverapi.py
+++ week06/naverapi.py
@@ -24,6 +24,10 @@ def main():
 
         start = jsonResponse['start'] + jsonResponse['display']
         jsonResponse = getNaverSearch(node, srcText, start, 100) # [CODE 2]
+        
+    print('전체 검색 : %d 건' %total)
+    
+    with open('%s_naver_%s.json' % (srcText, node), 'w', encoding=)
             
 def getNaverSearch(node, srcText, page_start, display):
     base = "https://openapi.naver.com/v1/search"
