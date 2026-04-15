37d62761f599661766f74f4db11c0cd3010293eb
nolr <nolr3@naver.com>
2026-04-15 22:26:08 +0900

    Auto commit: 2026-04-15 22:26 - 저장됨

--- week06/naverapi.py
+++ week06/naverapi.py
@@ -31,7 +31,7 @@ def getNaverSearch(node, srcText, page_start, display):
     parameters = "?query=%s&start=%s&display%s" % (urllib.parse.quote(srcText), page_start, display)
 
     url = base + node + parameters
-    responseDecode = getRequestUrl(url)
+    responseDecode = getRequestUrl(url)                 
 
     if (responseDecode == None):
         return None
