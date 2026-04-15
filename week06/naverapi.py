82ea483a8b795097925299fc7b98f73dabc6b408
nolr <nolr3@naver.com>
2026-04-15 22:23:51 +0900

    Auto commit: 2026-04-15 22:23 - 저장됨

--- week06/naverapi.py
+++ week06/naverapi.py
@@ -20,7 +20,7 @@ def main():
     while ((jsonResult != None) and (jsonResponse['display'] != 0)):
         for post in jsonResponse['items']:
             cnt += 1
-            getPostData(post, jsonResult, cnt)
+            getPostData(post, jsonResult, cnt)                # [CODE 3]
 
         start = jsonResponse['start'] + jsonResponse['display']
         jsonResponse = getNaverSearch(node, srcText, start, 100) # [CODE 2]
