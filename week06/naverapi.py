4d01dc0679d90d2b0e3c911716231c17f00acb13
nolr <nolr3@naver.com>
2026-04-15 22:36:37 +0900

    Auto commit: 2026-04-15 22:36 - 저장됨

--- week06/naverapi.py
+++ week06/naverapi.py
@@ -65,6 +65,7 @@ def getRequestUrl(url):
     except Exception as e:
         print(e)
         print("[%s] Error for URL : %s" % (datetime.datetime.now(), url))
+        return None
 
 
 def getPostData(post, jsonResult, cnt):  #[CODE 3]
