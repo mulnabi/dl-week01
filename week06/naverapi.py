155dcf4d7c4acf2707fac98be3bcb960770c1930
nolr <nolr3@naver.com>
2026-04-15 22:25:50 +0900

    Auto commit: 2026-04-15 22:25 - 저장됨

--- week06/naverapi.py
+++ week06/naverapi.py
@@ -8,7 +8,7 @@ client_secret = 'Client Secret'
 
 def main():
     
-    node = 'news'
+    node = 'news'                                             # 크롤링할 대상
     srcText = input('검색어를 입력하세요: ')
 
     cnt = 0
