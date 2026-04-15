f04e973d271fb98275606f842f4197cbfc2805cb
nolr <nolr3@naver.com>
2026-04-15 22:01:15 +0900

    Auto commit: 2026-04-15 22:01 - 저장됨

--- week06/naverapi.py
+++ week06/naverapi.py
@@ -20,4 +20,4 @@ def main():
     while ((jsonResult != None) and (jsonResponse['display'] != 0)):
         for post in jsonResponse['items']:
             cnt += 1
-            getPostData()
+            getPostData(post, jsonResult, cnt)
