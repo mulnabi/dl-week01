2659375b6883a70fcd4c8589d7979579cfa4e81f
nolr <nolr3@naver.com>
2026-04-16 09:20:28 +0900

    Auto commit: 2026-04-16 09:20 - 저장됨

--- week06/tourism.py
+++ week06/tourism.py
@@ -4,4 +4,9 @@ import datetime
 import json
 import pandas as pd
 
-ServiceKey = ""
+ServiceKey = "fe8d33d65644ac0b445f09784f017364ea502f4b1d2e74037f78e3442b72c1e4"
+
+"""### [CODE 0]"""
+
+def main():
+    
