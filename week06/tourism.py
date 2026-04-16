a02a9d24f85c1178fd5ea63928e9f946fa631ef9
nolr <nolr3@naver.com>
2026-04-17 05:20:21 +0900

    Auto commit: 2026-04-17 05:20 - 저장됨

--- week06/tourism.py
+++ week06/tourism.py
@@ -33,4 +33,5 @@ def getTourismStatsService(nat_cd, ed_cd, nStartYear, nEndYear):
     
     for year in range(nStartYear, nEndYear+1):
         for month in range(1, 13):
-            yyyymm = "{0}{1:0>2}".format()
+            yyyymm = "{0}{1:0>2}".format(str(year), str(month))
+            
