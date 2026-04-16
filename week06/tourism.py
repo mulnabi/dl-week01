e46fc7e3377795565eb5b5c90c5241379ab60a7a
nolr <nolr3@naver.com>
2026-04-17 05:22:23 +0900

    Auto commit: 2026-04-17 05:22 - 저장됨

--- week06/tourism.py
+++ week06/tourism.py
@@ -34,4 +34,5 @@ def getTourismStatsService(nat_cd, ed_cd, nStartYear, nEndYear):
     for year in range(nStartYear, nEndYear+1):
         for month in range(1, 13):
             yyyymm = "{0}{1:0>2}".format(str(year), str(month))
-            jsonData = getTourismStatsService
+            jsonData = getTourismStatsItem(yyyymm, nat_cd, ed_cd)     #[CODE 2]
+            if (jsonData['response']['header'])
