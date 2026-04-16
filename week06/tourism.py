b73956a717a1f9f2f868291ddee5ea520ff9dc8a
nolr <nolr3@naver.com>
2026-04-17 06:03:42 +0900

    Auto commit: 2026-04-17 06:03 - 저장됨

--- week06/tourism.py
+++ week06/tourism.py
@@ -76,4 +76,10 @@ def getTourismStatsItem(yyyymm, nat_cd, ed_cd):
 """### [CODE 1]"""
 
 def getRequestUrl(url):  #[CODE 1]
-    req = urllib.request.Request
+    req = urllib.request.Request(url)
+    try:
+        response = urllib.request.urlopen(req)
+        if response.getcode() == 200:
+            print("[%s] Url Request Success" % datetime.datetime.now())
+            return response.read().decode('utf-8')
+    except
