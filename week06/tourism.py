fbd49177626e141f9000774626b43dc828419a5f
nolr <nolr3@naver.com>
2026-04-17 05:59:28 +0900

    Auto commit: 2026-04-17 05:59 - 저장됨

--- week06/tourism.py
+++ week06/tourism.py
@@ -60,4 +60,10 @@ def getTourismStatsService(nat_cd, ed_cd, nStartYear, nEndYear):
 def getTourismStatsItem(yyyymm, nat_cd, ed_cd):
     Service_url = "http://openapi.tour.go.kr/openapi/service/EdrcntTourismStatsService/getEdrcntTourismStatsList"
     parameters = "?_type=json&serviceKey=" + ServiceKey       # 인증키
+    parameters += "&YM=" + yyyymm
+    parameters += "&NAT_CD=" + nat_cd
+    parameters += "&ED_CD=" + ed_cd
     
+    url = Service_url + parameters
+    
+    re
