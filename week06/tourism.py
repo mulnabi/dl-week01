dcdaa77b3a0256e9d03d27ed405e3708aa60d050
nolr <nolr3@naver.com>
2026-04-17 05:55:26 +0900

    Auto commit: 2026-04-17 05:55 - 저장됨

--- week06/tourism.py
+++ week06/tourism.py
@@ -47,4 +47,5 @@ def getTourismStatsService(nat_cd, ed_cd, nStartYear, nEndYear):
                 natName = jsonData['response']['body']['items']['item']['natKorNm']
                 natName = natName.replace(' ', '')
                 num = jsonData['response']['body']['items']['item']['num']
-                
+                ed = jsonData['response']['body']['items']['item']['ed']
+                print('[ %s_%s : %s ]' % (natName, yyyymm, num))
