fe803decaa058deb79833164812fefaae540690b
nolr <nolr3@naver.com>
2026-04-17 05:56:45 +0900

    Auto commit: 2026-04-17 05:56 - 저장됨

--- week06/tourism.py
+++ week06/tourism.py
@@ -52,3 +52,7 @@ def getTourismStatsService(nat_cd, ed_cd, nStartYear, nEndYear):
                 print('------------------------------------------------------')
                 jsonResult.append({'nat_name': natName, 'nat_cd': nat_cd, 'yyyymm': yyyymm, 'visit_cnt': num})
                 result.append([natName, nat_cd, yyyymm, num])
+    
+    return (jsonResult, result, natName, ed)
+
+""""""
