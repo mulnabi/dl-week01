a4211b7e02462a8e88f464b965a4ef15b5353a92
nolr <nolr3@naver.com>
2026-05-04 10:56:27 +0900

    Auto commit: 2026-05-04 10:56 - 저장됨

--- week07/classification.py
+++ week07/classification.py
@@ -29,3 +29,11 @@ plt.figure(figsize=(9, 9))
 for idx, image_data in enumerate(X[:100]):
     plt.subplot(10, 10, idx + 1)
     plot_digit(image_data)
+plt.subplots_adjust(wspace=0, hspace=0)
+plt.show()
+
+# train/test 데이터셋 나누기
+X_train, X_test, y_train, y_test = X[:60000], X[60000:], y[:60000], y[60000:]
+
+# 이진 분류기 훈련
+y
