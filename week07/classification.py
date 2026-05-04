507d9f3b2c9e7e4f102b524773ef952cb3fd61b6
nolr <nolr3@naver.com>
2026-05-04 10:31:24 +0900

    Auto commit: 2026-05-04 10:31 - 저장됨

--- week07/classification.py
+++ week07/classification.py
@@ -6,3 +6,7 @@ mnist = fetch_openml('mnist_784', as_frame=False)
 print(mnist.keys())  # data와 target만 사용
 
 X, y = mnist.data, mnist.target
+print(X)
+print(X.shape)       # 28 x 28 개의 픽셀 특징을 가진 이미지 70,000개
+print(y)
+print(y.shape)
