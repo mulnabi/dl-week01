072d7c716fad4245e91d458f960a76514fac2bb8
nolr <nolr3@naver.com>
2026-05-04 11:23:28 +0900

    Auto commit: 2026-05-04 11:23 - 저장됨

--- week07/classification.py
+++ week07/classification.py
@@ -60,4 +60,5 @@ print(any(dummy_clf.predict(X_train)))
 print(cross_val_score(dummy_clf, X_train, y_train_5, cv=3, scoring="accuracy"))
 
 # 오차 행렬
-from sklearn.m
+from sklearn.model_selection import cross_val_predict
+
