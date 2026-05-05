a9a8e5bb2c9950bc81aae2c759735ef8fa6d9c1e
nolr <nolr3@naver.com>
2026-05-05 22:02:21 +0900

    Auto commit: 2026-05-05 22:02 - 저장됨

--- data_analysis/netflix_project.py
+++ data_analysis/netflix_project.py
@@ -90,4 +90,10 @@ netflix = pd.read_csv('netflix_preprocessed.csv')
 sns.palplot(['#221f1f', '#b20710', '#e50914', '#f5f5f1'])
 
 # 제목 정하기
-plt.title('Netflix brand palette', loc='left', fontfamily='serif', fo)
+plt.title('Netflix brand palette', loc='left', fontfamily='serif', fontsize=15, y=1.2)
+plt.show()
+
+netflix['title'].str.contains('squid game', na=False, case=False)
+
+# 오징어 게임을 검색한 조건을 넷플릭스 데이터에 널어서 True인 값만 출력
+netflix[netflix['title'].str.contains('S')]
