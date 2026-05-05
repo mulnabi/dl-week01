630b7c34d4b6bf134d3329a798a9386542376387
nolr <nolr3@naver.com>
2026-05-05 19:31:58 +0900

    Auto commit: 2026-05-05 19:31 - 저장됨

--- week07/classification.py
+++ week07/classification.py
@@ -98,7 +98,7 @@ forest_clf = RandomForestClassifier(random_state=42)
 y_probas_forest = cross_val_predict(forest_clf, X_train, y_train_5, cv=3,
                                     method="predict_proba")
 
-y_probas_forest = y_probas_forest[:, 1]
+y_scores_forest = y_probas_forest[:, 1]
 y_train_pred_forest = y_probas_forest[:, 1] >= 0.5  # 양성 확률 ≥ 50%
 
 print(f1_score(y_train_5, y_train_pred_forest))
