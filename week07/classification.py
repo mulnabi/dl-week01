75737699a7c540f7fc2e8919f89d94484e98198e
nolr <nolr3@naver.com>
2026-05-05 16:26:12 +0900

    Auto commit: 2026-05-05 16:26 - 저장됨

--- week07/classification.py
+++ week07/classification.py
@@ -93,4 +93,6 @@ roc_auc_score(y_train_5, y_scores)
 #다음은 실행하는데 몇 분이 걸릴 수 있음
 from sklearn.ensemble import RandomForestClassifier
 
-forest
+forest_clf = RandomForestClassifier(random_state=42)
+
+y_
