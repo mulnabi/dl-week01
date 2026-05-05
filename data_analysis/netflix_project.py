6752a029a097330f31655d44753da6fa558b0ca1
nolr <nolr3@naver.com>
2026-05-05 22:00:03 +0900

    Auto commit: 2026-05-05 22:00 - 저장됨

--- data_analysis/netflix_project.py
+++ data_analysis/netflix_project.py
@@ -90,3 +90,4 @@ netflix = pd.read_csv('netflix_preprocessed.csv')
 sns.palplot(['#221f1f', '#b20710', '#e50914', '#f5f5f1'])
 
 # 제목 정하기
+plt.title('Netflix brand palette', loc='left', fontfamily='serif', fo)
