a11265983c1989cc8c613c8b251d4493d292d8e3
nolr <nolr3@naver.com>
2026-05-05 16:19:56 +0900

    Auto commit: 2026-05-05 16:19 - 저장됨

--- week07/classification.py
+++ week07/classification.py
@@ -75,4 +75,4 @@ print(confusion_matrix(y_train_5, y_train_perfect_predictions))
 # 정밀도와 재현율
 from sklearn.metrics import precision_score, recall_score
 
-print(precision_score(y_train_5, y_train_pred))
+print(precision_score(y_train_5, y_train_pred))  # == 3530 / ()
