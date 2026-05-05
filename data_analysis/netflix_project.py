825297956ce6fe41beec9e91725ffb734f96dd88
nolr <nolr3@naver.com>
2026-05-05 21:08:57 +0900

    Auto commit: 2026-05-05 21:08 - 저장됨

--- data_analysis/netflix_project.py
+++ data_analysis/netflix_project.py
@@ -29,5 +29,5 @@ for i in netflix.columns :
 # 결측치 비율 : country(9.44%)
 netflix['country'] = netflix['country'].fillna('No Data')
 
-# .replace(np.nan, 'b') : 
+# .replace(np.nan, 'b') : 결측치를 문자열로 바꾸기 함수를 통해 처리
 # 결측치 비율 : 
