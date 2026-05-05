955d7bfc69a553daa49aac834e29a6bd86d37c3e
nolr <nolr3@naver.com>
2026-05-05 21:29:58 +0900

    Auto commit: 2026-05-05 21:29 - 저장됨

--- data_analysis/netflix_project.py
+++ data_analysis/netflix_project.py
@@ -75,7 +75,8 @@ netflix['age_group'] = netflix['age_group'].map(age_group_dic)
 netflix.head(2)
 
 # 데이터 전처리 완료한 데이터셋 csv 파일로 저장
-#
+# index=False: 데이터프레임의 인덱스 열을 포함하지 않겠다는 뜻
+
 
 import numpy as np
 import pandas as pd
