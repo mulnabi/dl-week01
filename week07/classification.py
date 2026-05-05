0703e453bc4de1b6e12175633404a1fa0a51b224
nolr <nolr3@naver.com>
2026-05-05 13:13:04 +0900

    Auto commit: 2026-05-05 13:13 - 저장됨

--- week07/classification.py
+++ week07/classification.py
@@ -67,4 +67,6 @@ y_train_pred = cross_val_predict(sgd_clf, X_train, y_train_5, cv=3)
 from sklearn.metrics import confusion_matrix
 
 cm = confusion_matrix(y_train_5, y_train_pred)
-print()
+print(cm)
+
+y_train_perfect_
