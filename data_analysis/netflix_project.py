d45f2beba0b3446a1d29a928113e0f73290015c0
nolr <nolr3@naver.com>
2026-05-05 22:55:23 +0900

    Auto commit: 2026-05-05 22:55 - 저장됨

--- data_analysis/netflix_project.py
+++ data_analysis/netflix_project.py
@@ -181,3 +181,5 @@ netflix_age_country_unstack
 plt.figure(figsize=(15, 5))
 
 # 사용자 정의 컬럼맵 만들기
+cmap = plt.matplotlib.colors.LinearSegmentedColormap.from_list('', ['#221f1f','#b20710','#f5f5f1'])
+
