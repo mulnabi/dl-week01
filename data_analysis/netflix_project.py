260e58652e3ecc9c0c45935b0e5e60b88b1fbe66
nolr <nolr3@naver.com>
2026-05-05 21:03:34 +0900

    Auto commit: 2026-05-05 21:03 - 저장됨

--- data_analysis/netflix_project.py
+++ data_analysis/netflix_project.py
@@ -1,4 +1,24 @@
 # -*- coding: utf-8 -*-
 # 넷플릭스 데이터 분석 프로젝트
 
-import 
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# 세션 저장소에 업로드한 csv 파일을 읽어 변수에 할당
+netflix = pd.read_csv('netflix_titles.csv')
+netflix.head()
+
+# .columns : 열 이름 확인
+list(netflix.columns)
+
+# .head(3) : 데이터의 처음 3개의 행 출력
+netflix.head(3)
+
+# .info() : 열에 대한 요약 정보 확인
+netflix.info()
+
+# 넷플릭스 결측치 비율 확인하기
+for i in netflix.columns :
+    missingValueRate = netflix[i].isna().sum() / len(netflix) * 100
