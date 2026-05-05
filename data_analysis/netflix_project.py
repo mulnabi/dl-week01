acafcfd34c99cf9e72d5a39e64f48d4c9ff8d585
nolr <nolr3@naver.com>
2026-05-05 22:49:29 +0900

    Auto commit: 2026-05-05 22:49 - 저장됨

--- data_analysis/netflix_project.py
+++ data_analysis/netflix_project.py
@@ -161,4 +161,6 @@ netflix_age_country_unstack = netflix_age_country.groupby('age_group')['country'
 netflix_age_country_unstack
 
 # 특정 나이 그룹에 따른 특정 나라별 콘텐츠로 필터링
-# 연령, 국
+# 연령, 국가 리스트
+age_order = ['All', 'Older Kids', 'Teens', 'Adults']
+country_order = ['United States', 'India', 'United Kingdom',]
