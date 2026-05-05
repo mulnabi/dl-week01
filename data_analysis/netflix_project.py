1ca2081a4c9680a344a2404742daaea2c1298773
nolr <nolr3@naver.com>
2026-05-05 22:58:27 +0900

    Auto commit: 2026-05-05 22:58 - 저장됨

--- data_analysis/netflix_project.py
+++ data_analysis/netflix_project.py
@@ -203,3 +203,4 @@ plt.figure(figsize=(15, 5))
 # wordcolud에서 작동할 수 있도록 데이터프레임을 list로 1차 변환시키고 str(문자열)로 2차 변환
 text = str(list(netflix['description']))
 
+# mask : 단어를 그릴 위치 설정, 흰색(#FFFFFF) 항목은 마스킹된 것으로 간주
