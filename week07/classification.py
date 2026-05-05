5528a8f289dc5123e0bc28c8fb9e4f284227b88b
nolr <nolr3@naver.com>
2026-05-05 19:24:31 +0900

    Auto commit: 2026-05-05 19:24 - 저장됨

--- week07/classification.py
+++ week07/classification.py
@@ -97,3 +97,4 @@ forest_clf = RandomForestClassifier(random_state=42)
 
 y_probas_forest = cross_val_predict(forest_clf, X_train, y_train_5, cv=3,
                                     method="predict_proba")
+
