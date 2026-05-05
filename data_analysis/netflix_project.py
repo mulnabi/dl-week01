e61b0b4accc8c0b6ed991438b4ec75f2c5a8c30b
nolr <nolr3@naver.com>
2026-05-05 21:19:57 +0900

    Auto commit: 2026-05-05 21:19 - 저장됨

--- data_analysis/netflix_project.py
+++ data_analysis/netflix_project.py
@@ -47,4 +47,8 @@ netflix.info()
 # isna() == isnull() : 결측치 값은 True 반환하고, 그 외에는 False 반환
 netflix.isna().sum()
 
-# 시청 등급 기준표를 참고하여 Netflix의 rating 변수를 이용한 age_group
+# 시청 등급 기준표를 참고하여 Netflix의 rating 변수를 이용한 age_group_dic 변수 생성
+# rating 컬럼의 값을 age_group이라는 새로운 컬럼으로 복사
+netflix['age_group'] = netflix['rating']
+
+# 시청 등급 기준표를 참고하여 Netflix의 rating 변수를
