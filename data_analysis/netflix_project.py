b0baa38993803b6c7e689a23b0d865838c5b8cfa
nolr <nolr3@naver.com>
2026-05-05 22:56:49 +0900

    Auto commit: 2026-05-05 22:56 - 저장됨

--- data_analysis/netflix_project.py
+++ data_analysis/netflix_project.py
@@ -191,3 +191,6 @@ plt.title('Here we see interesting differences between countries. Most shows in 
           fontsize=12, fontfamily='serif')
 plt.show()
 
+# 넷플릭스 데이터의 description 열 이용한 워드 클라우드 생성
+
+# WordCloud : 워드 클라우드 생성에 필요한 모듈
