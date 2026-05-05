8c25b67712deb30857c94f03668156045058ac84
nolr <nolr3@naver.com>
2026-05-05 22:54:49 +0900

    Auto commit: 2026-05-05 22:54 - 저장됨

--- data_analysis/netflix_project.py
+++ data_analysis/netflix_project.py
@@ -172,3 +172,6 @@ netflix_age_country_unstack = netflix_age_country_unstack.loc[age_order, country
 
 # 결측치 0으로 처리
 netflix_age_country_unstack = netflix_age_country_unstack.fillna(0)
+netflix_age_country_unstack
+
+# 나이 그룹에 따른 국가별 넷플릭스 콘텐츠 비율 구하기
