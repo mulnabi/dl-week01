e1d782e6ce5658b59d33a354d6ffa53b795fbbee
nolr <nolr3@naver.com>
2026-05-05 19:23:44 +0900

    Auto commit: 2026-05-05 19:23 - 저장됨

--- week07/classification.py
+++ week07/classification.py
@@ -95,4 +95,4 @@ from sklearn.ensemble import RandomForestClassifier
 
 forest_clf = RandomForestClassifier(random_state=42)
 
-y_probas_forest = cross_val_predict
+y_probas_forest = cross_val_predict(forest_clf, X_train, y_train_5)
