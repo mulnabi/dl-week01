510db9dcefee1f70ec3542d05a6a9c7ae1a74d89
nolr <nolr3@naver.com>
2026-05-05 23:01:51 +0900

    Auto commit: 2026-05-05 23:01 - 저장됨

--- data_analysis/netflix_project.py
+++ data_analysis/netflix_project.py
@@ -102,7 +102,7 @@ type_counts = netflix['type'].value_counts()
 print(type_counts)
 
 # 5 x 5 크기의 플롯 만들기
-plt.figure(fiugsize=(5, 5))
+plt.figure(figsize=(5, 5))
 
 plt.pie(type_counts, labels=type_counts.index, autopct='%0.f%%', startangle=100,
         explode=[0.05, 0.05], shadow=True, colors=['#b20710', '#221f1f'])
