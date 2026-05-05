0480078e2274ee2d38b75f6003f5bafa5f2e95ee
nolr <nolr3@naver.com>
2026-05-05 21:28:41 +0900

    Auto commit: 2026-05-05 21:28 - 저장됨

--- data_analysis/netflix_project.py
+++ data_analysis/netflix_project.py
@@ -77,7 +77,7 @@ netflix.head(2)
 # 데이터 전처리 완료란 데이터셋 불러오기
 netflix = pd.read_csv('netflix_preprocessed.csv')
 
-
+import numpy as np
 
 # 넷플릭스 브랜드 상징 색깔 시각화
 sns.palplot(['#221f1f', '#b20710', '#e50914', '#f5f5f1'])
