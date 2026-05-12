84ac33982efc80f3eeaea5264e43e3718055544e
nolr <nolr3@naver.com>
2026-05-12 22:57:42 +0900

    Auto commit: 2026-05-12 22:57 - 저장됨

--- week08/regression.py
+++ week08/regression.py
@@ -54,3 +54,10 @@ plt.figure(figsize=(6, 4))
 plt.plot(X, y, "b.")
 plt.xlabel("$x_1$")
 plt.ylabel("$y$", rotation=0)
+plt.axis([-3, 3, 0, 10])
+plt.grid()
+plt.show()
+
+from sklearn.preprocessing import PolynomialFeatures
+
+poly_features = PolynomialFeatures(degree=2, include_bias=False)
