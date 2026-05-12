738ce9d3ba795d03cd98270580a61b0ddbd0cd48
nolr <nolr3@naver.com>
2026-05-12 20:08:30 +0900

    Auto commit: 2026-05-12 20:08 - 저장됨

--- week08/regression.py
+++ week08/regression.py
@@ -13,3 +13,20 @@ np.random.seed(42)                        # 코드 예제를 재현 가능하게
 m = 100                                   # 샘플 개수
 X = 2 * np.random.rand(m, 1)              # 열 벡터
 y = 4 + 3 * X + np.random.randn(m, 1)     # 열 벡터
+
+import matplotlib.pyplot as plt
+
+plt.figure(figsize=(6, 4))
+plt.plot(X, y, "b.")
+plt.xlabel("$x_1$")
+plt.ylabel("$y$", rotation=0)
+plt.axis([0, 2, 0, 15])
+plt.grid()
+plt.show()
+
+from sklearn.linear_model import LinearRegression
+
+lin_reg = LinearRegression()
+lin_reg.fit(X, y)
+lin_reg.intercept_, lin_reg.coef_
+
