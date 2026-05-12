19bd6eabd33810cec8c973c78569dad49ab49331
nolr <nolr3@naver.com>
2026-05-12 23:17:11 +0900

    Auto commit: 2026-05-12 23:17 - 저장됨

--- week08/regression.py
+++ week08/regression.py
@@ -69,3 +69,5 @@ lin_reg = LinearRegression()
 lin_reg.fit(X_poly, y)
 lin_reg.intercept_, lin_reg.coef_
 
+X_new = np.linspace(-3, 3, 100).reshape(100, 1)
+X_new_poly = poly_features.transform(X_new)
