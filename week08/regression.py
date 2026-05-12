50603aab2fa2f026d441a3ac34e1642c54001bda
nolr <nolr3@naver.com>
2026-05-12 20:01:45 +0900

    Auto commit: 2026-05-12 20:01 - 저장됨

--- week08/regression.py
+++ week08/regression.py
@@ -1,2 +1,13 @@
 # -*- coding: utf-8 -*-
-import 
+import matplotlib.pyplot as plt
+
+plt.rc('font', size=14)
+plt.rc('axes', labelsize=14, titlesize=14)
+plt.rc('legend', fontsize=14)
+plt.rc('xtick', labelsize=10)
+plt.rc('ytick', labelsize=10)
+
+import numpy as np
+
+np.random.seed(42)
+m = 100
