dc1a82fe6a78994348efcaab58e39b365b560f94
nolr <nolr3@naver.com>
2026-05-13 21:21:33 +0900

    Auto commit: 2026-05-13 21:21 - 저장됨

--- week08/svm.py
+++ week08/svm.py
@@ -15,4 +15,6 @@ svm_clf = make_pipeline(StandardScaler(),
                         LinearSVC(C=1, dual=True, random_state=42))
 svm_clf.fit(X, y)
 
-X
+X_new = [[5.5, 1.7], [5.0, 1.5]]
+
+print(vsm_clf.predict(X_new))
