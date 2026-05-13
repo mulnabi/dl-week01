bca38418e0e8282d64cf28a547fd8646f1fafe22
nolr <nolr3@naver.com>
2026-05-13 10:18:15 +0900

    Auto commit: 2026-05-13 10:18 - 저장됨

--- week08/svm.py
+++ week08/svm.py
@@ -5,4 +5,12 @@ import numpy as np
 from sklearn.datasets import load_iris
 from sklearn.pipeline import make_pipeline
 from sklearn.preprocessing import StandardScaler
+from sklearn.svm import LinearSVC
 
+iris = load_iris(as_frame=True)
+X = iris.data[["petal length (cm)", "petal width (cm)"]].values
+y = (iris.target == 2)  # Iris virginica
+
+svm_clf = make_pipeline(StandardScaler(),
+                        LinearSVC(C=1, dual=True, random_state=42))
+svm_clf.fit(X, y)
