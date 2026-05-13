2e30b9596e620a5ac419ef49230d86c8bf748f1e
nolr <nolr3@naver.com>
2026-05-13 10:09:47 +0900

    Auto commit: 2026-05-13 10:09 - 저장됨

--- week08/regression.py
+++ week08/regression.py
@@ -149,3 +149,5 @@ from sklearn.model_selection import train_test_split
 
 X = iris.data[["petal width (cm)"]].values
 y = iris.target_names[iris.target] == 'virginica'
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42)
+
