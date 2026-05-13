e129c4e2ba2db012b69fd8f8603cd198a4d51ded
nolr <nolr3@naver.com>
2026-05-13 22:20:01 +0900

    Auto commit: 2026-05-13 22:20 - 저장됨

--- week08/svm.py
+++ week08/svm.py
@@ -61,3 +61,6 @@ from sklearn.svm import SVR
 np.random.seed(42)
 X = 2 * np.random.rand(50, 1) - 1
 y = 0.2 + 0.1 * X[:, 0] + 0.5 * X[:, 0] ** 2 + np.random.randn(50) / 10
+
+svm_poly_reg = make_pipeline(StandardScaler(),
+                             
