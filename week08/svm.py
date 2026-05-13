11dcc58c10a19d4be61463469bd755d46cc3b355
nolr <nolr3@naver.com>
2026-05-13 22:16:45 +0900

    Auto commit: 2026-05-13 22:16 - 저장됨

--- week08/svm.py
+++ week08/svm.py
@@ -47,3 +47,12 @@ poly_kernel_svm_clf.fit(X, y)
 from sklearn.svm import LinearSVR
 
 # 이 세 줄은 간단한 선형 데이터셋을 생성합니다.
+np.random.seed(42)
+X = 2 * np.random.rand(50, 1)
+y = 4 + 3 * X[:, 0] + np.random.randn(50)
+
+svm_reg = make_pipeline(StandardScaler(),
+                        LinearSVR(epsilon=0.5, dual=True, random_state=42))
+svm_reg.fit(X, y)
+
+from sklearm.svm import SVR
