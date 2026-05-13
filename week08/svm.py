d53a0d6bd29272b8dded43b1b70a7f019dde8b83
nolr <nolr3@naver.com>
2026-05-13 22:20:30 +0900

    Auto commit: 2026-05-13 22:20 - 저장됨

--- week08/svm.py
+++ week08/svm.py
@@ -64,5 +64,4 @@ y = 0.2 + 0.1 * X[:, 0] + 0.5 * X[:, 0] ** 2 + np.random.randn(50) / 10
 
 svm_poly_reg = make_pipeline(StandardScaler(),
                              SVR(kernel="poly", degree=2, C=0.01, epsilon=0.1))
-
 svm_poly_reg.fit(X, y)
