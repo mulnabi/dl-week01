c38adc408a2d5e0d78d81a30618f018197ff32b1
nolr <nolr3@naver.com>
2026-05-13 21:23:34 +0900

    Auto commit: 2026-05-13 21:23 - 저장됨

--- week08/svm.py
+++ week08/svm.py
@@ -17,4 +17,15 @@ svm_clf.fit(X, y)
 
 X_new = [[5.5, 1.7], [5.0, 1.5]]
 
-print(vsm_clf.predict(X_new))
+print(svm_clf.predict(X_new))
+print(svm_clf.decision_function(X_new))
+
+# 비선형 SVM 분류
+
+from sklearn.datasets import make_moons
+from sklearn.preprocessing import PolynomialFeatures
+
+X, y = make_moons(n_samples=100, noise=0.15, random_state=42)
+
+PolynomialFeatures(degree=3,
+                   )
