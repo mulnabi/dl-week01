549e8e2bc5309f5ff05fe4b51a61fccbe6ea003d
nolr <nolr3@naver.com>
2026-05-13 10:09:26 +0900

    Auto commit: 2026-05-13 10:09 - 저장됨

--- week08/regression.py
+++ week08/regression.py
@@ -142,4 +142,9 @@ list(iris)
 print(iris.data.head(3))
 print(iris.target.head(3))  # 샘플이 섞여 있지 않습니다.
 
-print(iris.t)
+print(iris.target_names)
+
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+
+X = iris.data[["petal width (cm)"]].values
