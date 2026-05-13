3e0699f7fa0325ee7f7c3d904865ecc34daa8dff
nolr <nolr3@naver.com>
2026-05-13 10:09:40 +0900

    Auto commit: 2026-05-13 10:09 - 저장됨

--- week08/regression.py
+++ week08/regression.py
@@ -148,3 +148,4 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 
 X = iris.data[["petal width (cm)"]].values
+y = iris.target_names[iris.target] == 'virginica'
