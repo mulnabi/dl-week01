71879330776372fc8379301070278f6271e9f813
nolr <nolr3@naver.com>
2026-05-27 07:37:57 +0900

    Auto commit: 2026-05-27 07:37 - 저장됨

--- week09/decision_tree.py
+++ week09/decision_tree.py
@@ -2,4 +2,6 @@ from sklearn.datasets import load_iris
 from sklearn.tree import DecisionTreeClassifier
 
 iris = load_iris(as_frame=True)
-X_iris = iris.data[]
+X_iris = iris.data[["petal length (cm)", "petal width (cm)"]].values
+y_iris = iris.target
+
