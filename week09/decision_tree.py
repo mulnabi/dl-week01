62fcda6eb54cad15105cf78532b5c790a0cf4ba9
nolr <nolr3@naver.com>
2026-05-27 10:46:13 +0900

    Auto commit: 2026-05-27 10:46 - 저장됨

--- week09/decision_tree.py
+++ week09/decision_tree.py
@@ -5,7 +5,7 @@ iris = load_iris(as_frame=True)
 X_iris = iris.data[["petal length (cm)", "petal width (cm)"]].values
 y_iris = iris.target
 
-tree_clf = DecisionTreeClassifier(mxa_depth=2, ramdom_state=42)
+tree_clf = DecisionTreeClassifier(max_depth=2, ramdom_state=42)
 tree_clf.fit(X_iris, y_iris)
 
 from sklearn.tree import export_graphviz
