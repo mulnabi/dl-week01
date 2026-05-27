4ff528e867000bda67f0c6e882dd9d4ac8ce9085
nolr <nolr3@naver.com>
2026-05-27 09:30:45 +0900

    Auto commit: 2026-05-27 09:30 - 저장됨

--- week09/decision_tree.py
+++ week09/decision_tree.py
@@ -36,4 +36,10 @@ X_moons, y_moons = make_moons(n_samples=150, noise=0.2, random_state=42)
 
 tree_clf1 = DecisionTreeClassifier(random_state=42)
 tree_clf2 = DecisionTreeClassifier(min_samples_leaf=5, random_state=42)
-tree_clf1
+tree_clf1.fit(X_moons, y_moons)
+tree_clf2.fit(X_moons, y_moons)
+
+X_moons_test, y_moons_test = make_moons(n_samples=1000, noise=0.2,
+                                        random_state=43)
+
+print(tree_clf1.sc)
