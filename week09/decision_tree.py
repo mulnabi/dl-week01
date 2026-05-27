65f98bc23153f5e647de4ca2d1711daac727d612
nolr <nolr3@naver.com>
2026-05-27 10:17:26 +0900

    Auto commit: 2026-05-27 10:17 - 저장됨

--- week09/decision_tree.py
+++ week09/decision_tree.py
@@ -60,4 +60,9 @@ tree_reg.fit(X_quad, y_quad)
 export_graphviz(
     tree_reg,
     out_file="regression_tree.dot"
+    feature_names=["x1"],
+    rounded=True,
+    filled=True
 )
+Source.from_file("regression_tree.dot")
+
