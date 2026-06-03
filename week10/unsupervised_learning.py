d79a233f9ced6c742f8f94da1636620f0c9f36dd
nolr <nolr3@naver.com>
2026-06-03 17:30:38 +0900

    Auto commit: 2026-06-03 17:30 - 저장됨

--- week10/unsupervised_learning.py
+++ week10/unsupervised_learning.py
@@ -102,3 +102,7 @@ for k in (3, 4, 5, 6):
     
     plt.gca().yaxis.set_major_locator(FixedLocator(ticks))
     plt.gca().yaxis.set_major_formatter(FixedFormatter(range(k)))
+    if k in (3, 5):
+        plt.ylabel("클러스터")
+    
+    if k in (3, 5):
