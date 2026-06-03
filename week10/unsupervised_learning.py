82b16b6514d92ce53d60c4726cb8f70a28d34b15
nolr <nolr3@naver.com>
2026-06-03 18:57:15 +0900

    Auto commit: 2026-06-03 18:57 - 저장됨

--- week10/unsupervised_learning.py
+++ week10/unsupervised_learning.py
@@ -66,10 +66,10 @@ from sklearn.metrics import silhouette_score
 
 print(silhouette_score(X, kmeans.labels_))
 
-silhouette = [silhouette_score(X, model.labels_) for model in kmeans_per_k[1:]]
+silhouette_scores = [silhouette_score(X, model.labels_) for model in kmeans_per_k[1:]]
 
 plt.figure(figsize=(8, 3))
-plt.plot(range(2, 10), silhouettes, "bo-")
+plt.plot(range(2, 10), silhouette_scores, "bo-")
 plt.xlabel("$k$")
 plt.ylabel("실루엣 점수")
 plt.axis([1.8, 8.5, 0.55, 0.8])
