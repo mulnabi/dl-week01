f12bf252a9372e45a98ad17a3e8cf632439c3330
nolr <nolr3@naver.com>
2026-06-03 17:21:08 +0900

    Auto commit: 2026-06-03 17:21 - 저장됨

--- week10/unsupervised_learning.py
+++ week10/unsupervised_learning.py
@@ -93,3 +93,7 @@ for k in (3, 4, 5, 6):
     for i in range(k):
         coeffs = silhouette_coefficients[y_pred == i]
         coeffs.sort()
+        
+        color = plt.cm.Spectral(i / k)
+        plt.fill_betweenx(np.arange(pos, pos + len(coeffs)), 0, coeffs,
+                          facecolor=color, edgecolor=color, alpha=0.7)
