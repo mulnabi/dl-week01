890e2d1cf42af83f1a523f650094768c46eeef16
nolr <nolr3@naver.com>
2026-06-03 17:53:03 +0900

    Auto commit: 2026-06-03 17:53 - 저장됨

--- week10/unsupervised_learning.py
+++ week10/unsupervised_learning.py
@@ -111,4 +111,5 @@ for k in (3, 4, 5, 6):
     else:
         plt.tick_params(labelbottom=False)
     
-    
+    plt.axvline(x=silhouette_scores[k - 2], color="red", linestyle="--")
+    plt.title(f"$k={k}$")
