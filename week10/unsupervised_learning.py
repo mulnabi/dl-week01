a8e9a8cd3a38aa3df0f8bda44db7deba2fbe7661
nolr <nolr3@naver.com>
2026-06-03 13:44:26 +0900

    Auto commit: 2026-06-03 13:44 - 저장됨

--- week10/unsupervised_learning.py
+++ week10/unsupervised_learning.py
@@ -13,3 +13,6 @@ plt.rc('ytick', labelsize=10)
 # K-평균
 from sklearn.cluster import KMeans
 from sklearn.datasets import make_blobs
+import numpy as np
+
+blob_centers = np.array([[ 0.2, 2.3], [-1.5 , ]])
