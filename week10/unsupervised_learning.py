ba013ac7c6be5d6341389c5096d375c96a553c0d
nolr <nolr3@naver.com>
2026-06-03 17:30:08 +0900

    Auto commit: 2026-06-03 17:30 - 저장됨

--- week10/unsupervised_learning.py
+++ week10/unsupervised_learning.py
@@ -100,4 +100,5 @@ for k in (3, 4, 5, 6):
         ticks.append(pos + len(coeffs) // 2)
         pos += len(coeffs) + padding
     
-    plt.gca().yaxis.set_major_locator
+    plt.gca().yaxis.set_major_locator(FixedLocator(ticks))
+    plt.gca().yaxis.set_major_formatter(FixedFormatter(range(k)))
