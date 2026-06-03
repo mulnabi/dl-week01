4468037fc62f07515ac0d1189e1102ddc212219e
nolr <nolr3@naver.com>
2026-06-03 17:06:52 +0900

    Auto commit: 2026-06-03 17:06 - 저장됨

--- week10/unsupervised_learning.py
+++ week10/unsupervised_learning.py
@@ -59,3 +59,9 @@ plt.ylabel("이너셔")
 plt.annotate("", xy=(5, inertias[4]), xytext=(4.45, 650), arrowprops=dict(facecolor='black', shrink=0.1))
 plt.text(4.5, 650, "엘보", horizontalalignment="center")
 plt.axis([1, 8.5, 0, 3500])
+plt.grid()
+plt.show()
+
+from sklearn.metrics import silhouette_score
+
+print(silhouette_score(X, kmeans.labels_))
