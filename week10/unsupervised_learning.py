83f7b1a97e09f52fe53a8271a5811c039d4cae40
nolr <nolr3@naver.com>
2026-06-03 13:45:53 +0900

    Auto commit: 2026-06-03 13:45 - 저장됨

--- week10/unsupervised_learning.py
+++ week10/unsupervised_learning.py
@@ -15,4 +15,7 @@ from sklearn.cluster import KMeans
 from sklearn.datasets import make_blobs
 import numpy as np
 
-blob_centers = np.array([[ 0.2, 2.3], [-1.5 , ]])
+blob_centers = np.array([[ 0.2, 2.3], [-1.5 ,  2.3], [-2.8, 2.8],
+                         [-2.8, 4.2], [-2.8, 1.0]])
+blob_std = np.array([0.4, 0.3, 0.1, 0.1, 0.1])
+X, y = make_blobs()
