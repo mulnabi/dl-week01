d31c0e78e26a66e7578b1e199a827e6e85c9a553
nolr <nolr3@naver.com>
2026-06-03 13:48:20 +0900

    Auto commit: 2026-06-03 13:48 - 저장됨

--- week10/unsupervised_learning.py
+++ week10/unsupervised_learning.py
@@ -19,4 +19,8 @@ blob_centers = np.array([[ 0.2, 2.3], [-1.5 ,  2.3], [-2.8, 2.8],
                          [-2.8, 4.2], [-2.8, 1.0]])
 blob_std = np.array([0.4, 0.3, 0.1, 0.1, 0.1])
 X, y = make_blobs(n_samples=2000, centers=blob_centers, cluster_std=blob_std,
-                  )
+                  random_state=7)
+
+k = 5
+kmeans = KMeans(n_clusters=k, n_init=10, random_state=42)
+y_pred = kmeans.fit_predict(x)
