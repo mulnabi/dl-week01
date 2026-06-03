c006a28921c20f1c0a7e343e03fd07515d278a9e
nolr <nolr3@naver.com>
2026-06-03 18:23:27 +0900

    Auto commit: 2026-06-03 18:23 - 저장됨

--- week10/unsupervised_learning.py
+++ week10/unsupervised_learning.py
@@ -139,4 +139,4 @@ def plot_dbscan(dbscan, X, size, show_xlabels=True, show_ylabels=True):
     anomalies = X[anomalies_make]
     non_cores = X[non_core_make]
     
-    plt.scatter(cores[:, 0], cores[:, 1], )
+    plt.scatter(cores[:, 0], cores[:, 1], c=dbscan.labels_[core_make], marker='o', s=size, cmap="Paired")
